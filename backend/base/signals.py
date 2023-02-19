6c70ea96c082484e95d3529ae9feb8ba2ab3349c
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2023-02-19 21:16:38 +0400

    Update signals.py

--- backend/base/signals.py
+++ backend/base/signals.py
@@ -20,20 +20,24 @@ def send_order_email(sender, instance, created, **kwargs):
     order_id = instance.id
     if created:
         subject = f'Your order #{order_id} has been received!'
-        message = f'Thank you for your order. We will update you when it is ready for delivery. You can view your order details here: http://localhost:3000/order/{order_id}'
+        message = f'Thank you for your order. We will update you when it is ready for delivery. You can view your order details here:\n\nhttp://localhost:3000/order/{order_id}'
+
+        from_email = 'johndoe@example.com'
+        recipient_list = [instance.user.email, from_email] 
+        send_mail(subject, message, from_email, recipient_list)
     else:
         if instance.is_paid:
             subject = 'Thank you for your payment!'
-            message = f'Your order #{order_id} has successfully been paid! Hope you shop again soon! Paid at: {instance.paid_at}. You can view your order details here: http://localhost:3000/order/{order_id}'
+            message = f'Your order #{order_id} has successfully been paid! Hope you shop again soon! Paid at: {instance.paid_at}.\n\nYou can view your order details here: http://localhost:3000/order/{order_id}'
         elif instance.is_delivered:
             subject = f'Your order #{order_id} has been delivered!'
-            message = f'We hope you enjoy your purchase. Please let us know if you have any issues. Delivered at: {instance.delivered_at}. You can view your order details here: http://localhost:3000/order/{order_id}'
+            message = f'We hope you enjoy your purchase. Please let us know if you have any issues. Delivered at: {instance.delivered_at}.\n\nYou can view your order details here: http://localhost:3000/order/{order_id}'
         elif instance.delivery_eta:
             subject = 'Your order has a new delivery estimate'
-            message = f'Your order is now estimated to arrive on {instance.delivery_eta}. We will notify you if this changes. You can view your order details here: http://localhost:3000/order/{order_id}'
+            message = f'Your order is now estimated to arrive on {instance.delivery_eta}. We will notify you if this changes.\n\nYou can view your order details here: http://localhost:3000/order/{order_id}'
         else:
             return
         
-        from_email = 'example@email.com'
-        recipient_list = [instance.user.email] 
+        from_email = 'johndoe@example.com'
+        recipient_list = [instance.user.email, from_email] 
         send_mail(subject, message, from_email, recipient_list)
